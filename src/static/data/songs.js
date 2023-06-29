7b4cc65416c806f71dce3e43bed704393309da72
Shira Chadad <s0504173725@gmail.com>
2023-06-29 13:47:50 +0300

    edit id in songs to _id

--- src/static/data/songs.js
+++ src/static/data/songs.js
@@ -1,6 +1,6 @@
 export const items = [
     {
-      id:1,
+      _id:1,
       title:'#tbt',
       subtitle:'Zamansız, nostajik',
       image: 'https://i.scdn.co/image/ab67706f000000028c0611904a4a794a0a07136f',
@@ -8,7 +8,7 @@ export const items = [
       src: "https://cdn.freesound.org/previews/646/646718_922322-lq.mp3"
     },
     {
-      id:2,
+      _id:2,
       title:'Pozitif',
       subtitle:'Good vibes only',
       image:'https://i.scdn.co/image/ab67706f0000000284d1ff2af2a9e1cbac76cacd',
@@ -16,7 +16,7 @@ export const items = [
       src:"https://cdn.freesound.org/previews/646/646717_1015240-lq.mp3"
     },
     {
-      id:3,
+      _id:3,
       title:'altüst',
       subtitle:'Biraz dağıldık gibi',
       image:'https://i.scdn.co/image/ab67706f0000000246cd2f075db478c596975cef',
@@ -24,7 +24,7 @@ export const items = [
       src:"https://cdn.freesound.org/previews/646/646715_1648170-lq.mp3"
     },
     {
-      id:4,
+      _id:4,
       title:'trend',
       subtitle:'İnternette olan biten',
       image:'https://i.scdn.co/image/ab67706f0000000226b4c3a3b4a8469fe5cb9591',
@@ -32,7 +32,7 @@ export const items = [
       src:"https://cdn.freesound.org/previews/646/646710_5187472-lq.mp3"
     },
     {
-      id:5,
+      _id:5,
       title:'Hot Hits Turkey',
       subtitle:'Dinlemekten vazgeçemediklerin ve en yeni hitler. Kapak: Ezhel',
       image:'https://i.scdn.co/image/ab67706f00000002eddd8a9a4f34a27ac5cb19a4',
@@ -42,7 +42,7 @@ export const items = [
   ]
 export const items2 = [
     {
-      id:6,
+      _id:6,
       title:'Maksimum Konsantrasyon',
       subtitle:'Mükemmel piyano melodileri ile verimli dakikalar.',
       image: 'https://i.scdn.co/image/ab67706f00000002d8e55d32c635c4a634ef689d',
@@ -50,7 +50,7 @@ export const items2 = [
       src:"https://cdn.freesound.org/previews/646/646705_5674468-lq.mp3"
     },
     {
-      id:7,
+      _id:7,
       title:'Piano Study',
       subtitle:'Music to help you concentrate during your homework.',
       image:'https://i.scdn.co/image/ab67706f000000029cf07006203e941cb469c681',
@@ -58,7 +58,7 @@ export const items2 = [
       src:"https://cdn.freesound.org/previews/646/646705_5674468-lq.mp3"
     },
     {
-      id:8,
+      _id:8,
       title:'Peaceful Piano',
       subtitle:'Relax and indulge with beautiful piano pieces',
       image:'https://i.scdn.co/image/ab67706f00000002ca5a7517156021292e5663a6',
@@ -66,7 +66,7 @@ export const items2 = [
       src:"https://cdn.freesound.org/previews/646/646705_5674468-lq.mp3"
     },
     {
-      id:9,
+      _id:9,
       title:'lofi beats',
       subtitle:'The chillest beats to help you relax, study, code, and focus.',
       image:'https://i.scdn.co/image/ab67706f00000002abaf6c3c6a4b29f8a4565a86',
@@ -74,7 +74,7 @@ export const items2 = [
       src:"https://cdn.freesound.org/previews/646/646705_5674468-lq.mp3"
     },
     {
-      id:10,
+      _id:10,
       title:'Deep Focus',
       subtitle:'Keep calm and focus with ambient and post-rock music.',
       image:'https://i.scdn.co/image/ab67706f000000025551996f500ba876bda73fa5',
@@ -84,7 +84,7 @@ export const items2 = [
   ]
 export const items3 = [
     {
-      id:11,
+      _id:11,
       title:'Evde',
       subtitle:'Favori adresin',
       image: 'https://i.scdn.co/image/ab67706f00000002669519d077903ef3b9e81b57',
@@ -92,7 +92,7 @@ export const items3 = [
       src:"https://cdn.freesound.org/previews/646/646710_5187472-lq.mp3"
     },
     {
-      id:12,
+      _id:12,
       title:'Ev Yapımı',
       subtitle:'Evin en keyifli köşesi: mutfak',
       image:'https://i.scdn.co/image/ab67706f000000023b378485f17f98776b66c2aa',
@@ -100,7 +100,7 @@ export const items3 = [
       src:"https://cdn.freesound.org/previews/646/646715_1648170-lq.mp3"
     },
     {
-      id:13,
+      _id:13,
       title:'Ev Ofis',
       subtitle:'Evden çalışırken müzik dinlemeyi sevenlere',
       image:'https://i.scdn.co/image/ab67706f00000002a436e3f6117f461ea879c8a2',
@@ -108,7 +108,7 @@ export const items3 = [
       src:"https://cdn.freesound.org/previews/646/646715_1648170-lq.mp3"
     },
     {
-      id:14,
+      _id:14,
       title:'Evde Spor',
       subtitle:'Egzersizden vazgeçme',
       image:'https://i.scdn.co/image/ab67706f00000002c357b8c50095c4d094cfb64e',
@@ -116,7 +116,7 @@ export const items3 = [
       src:"https://cdn.freesound.org/previews/646/646715_1648170-lq.mp3"
     },
     {
-      id:15,
+      _id:15,
       title:'Tertemiz',
       subtitle:'Pırıl pırıl bir ev için',
       image:'https://i.scdn.co/image/ab67706f00000002c4871378397524380558d8ed',
